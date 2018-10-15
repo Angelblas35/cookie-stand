f06c73f9fd05881c82a96767a0176b91d70500ea
angel <angelblas17@gmail.com>
2018-10-15 15:25:28 -0700

    added some content

---
+++ js/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var store= {
+     days: ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday']
+     hours: []
+
+var locations: ['Downtown', 'Ballard', ]
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random()) * Math.floor(max);
+}
