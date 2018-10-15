b98399c9fe7a797604bd14616fc8d361c958ed18
angel <angelblas17@gmail.com>
2018-10-15 16:04:07 -0700

    fixed objects

--- js/app.js
+++ js/app.js
@@ -1,12 +1,47 @@
 'use strict';
 
-var store= {
-     days: ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday']
-     hours: []
-
-var locations: ['Downtown', 'Ballard', ]
+var openHours = ['8am','9am'] 
+var firstAndPike= {
+  minCustomersPerHour: 23,
+  maxCustomersPerHour: 60,
+  averageCookiesPerCustomer: 6.3,
+  randomCustomersPerHour: function(min, max) {
+    
+    return true;
+  },
+  getCookiesPerHour: function(){
+    // for (let i = 0; i < openHours.length; i++) {
+    // var customers = array[i];
+    // this.cookiesPerHour.push(randomCustomersPerHour() * this.averageCookiePerCustomer)
+    // }
+  },
+  customersPerHour: [],
+  cookiesPerHour: [],
 }
 
-function getRandomInt(max) {
-    return Math.floor(Math.random()) * Math.floor(max);
-}
+// var storeTwo= {
+//     name: 'SeaTac Airport'
+//     days: ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday']
+
+// }
+
+// var storeThree= {
+//     name: 'Seattle Center'
+//     days: ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+// }
+
+// var storeFour= {
+//     name= 'Capitol Hill'
+
+// }
+
+// var storeFive= {
+//     name= 'Alki'
+// }
+
+// function getRandomInt(max) {
+//     return Math.floor(Math.random()) * Math.floor(max);
+// }
+
+console.log(firstAndPike.randomCustomersPerHour())
+console.log(firstAndPike.getCookiesPerHour())
